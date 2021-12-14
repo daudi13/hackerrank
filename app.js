473c0e565b0990eb4599c3b8f386a595bd31df09
daudi13 <davouma13@gmail.com>
2021-12-14 04:43:33 +0300

    commit question

--- app.js
+++ app.js
@@ -293,3 +293,13 @@ const timeConversion = function (s) {
 
 
 timeConversion('07:05:45PM')
+
+
+/*HackerLand University has the following grading policy:
+Every student receives a grade in the inclusive range from 0 to 100, any grade less than 40 is a failing grade.
+
+Sam is a professor at the university and likes to round each student's grade according to these rules:
+
+if the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5
+
+if the value of grade is less than 38, no rounding occurs as the result will be a failing grade*/
