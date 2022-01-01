780626ae8f854306e635a659c3ef1e7f3bd2d092
daudi13 <davouma13@gmail.com>
2022-01-01 15:53:52 +0300

    commit changes

--- app.js
+++ app.js
@@ -520,3 +520,33 @@ designerPdfViewer(h, 'zaba')
 
 /*given an array of integers, find the longest subarray where the absolute difference between any two elements is less than or equal to 1*/
 
+
+
+const pickingNumbers = function (arr) {
+
+    const newPt = [];
+    const element = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        
+
+        for (let j = i + 1; j < arr.length; j++) {
+
+            if (Math.abs(arr[i] - arr[j] )=== 1) {
+                newPt.push(arr[i])
+                element.push(arr[j])
+
+                if (element[i] - newPt[i] === 1) {
+                    console.log(element[i], newPt[i])
+                }
+            }
+        }
+    }
+
+    console.log(newPt);
+    console.log(element)
+}
+
+
+const a = [1, 1, 2, 2, 4, 4, 5, 5, 5];
+pickingNumbers(a);
