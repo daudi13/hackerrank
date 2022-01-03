8ede0a15db93ca30e98bdb2f821ade04089981e2
daudi13 <davouma13@gmail.com>
2022-01-03 06:56:52 +0300

    commit changes

--- app.js
+++ app.js
@@ -550,3 +550,16 @@ const pickingNumbers = function (arr) {
 
 const a = [1, 1, 2, 2, 4, 4, 5, 5, 5];
 pickingNumbers(a);
+
+/*Marie inveted a time machine and wants to test it by time-traveling to visit russia  on the Day of the programmer(the 256th day of the year) during a year in the inclusive rane from 1700 to 2700. From 1700 to 1917, Russia's official calendar was the julian calendar; since 1919, when the next day after january 31st was February 14th. this means that in 1918, February 14th was the 32nd day of russia. In both calendar systems, Febraury is the only month with a variable amount of days; It has 29days during a leap year and 28 days during all other years. In the Julian calendar, Leap years are divisible by 4; in the Gregorian calendar, leap years are either of the following
+
+Divisible by 400.
+Divisible by 4 and not divisible by 100.
+
+Given a year, y find the date of the 256th day of that year according to the official Russian calendar during that year. then print it in the format dd.mm.yyyy where dd is the two-digit day, mm is the two-digit month, and yyyy is the y.
+For example, the given year = 1984. 1984 is divisible by 4, so it is a leap year. the 256th day of a leap year after 1918 is september 12, so the answer is 12.09.1984
+
+
+
+
+*/
