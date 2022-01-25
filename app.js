9f3185e46bdcd5a9b414b51d8ed182727c451c97
daudi13 <davouma13@gmail.com>
2022-01-25 06:07:38 +0300

    Add viralAdverstising function

--- app.js
+++ app.js
@@ -732,3 +732,26 @@ const sumAllOddFibs = num => {
 }
 
 console.log(sumAllOddFibs(10))
+
+/*HackerLand Enterprise is adopting a new viral advertising strategy. When they launch a new product, they advertise it to exactly  5 people on social media.
+
+On the first day, half of those 5 people (i.e., floor(5/2) = 2) like the advertisement and each shares it with  3 of their friends. At the beginning of the second day, floor(5/2) * 3 = 2 * 3 = 6 people receive the advertisement.
+Each day, floor(recipient/2) of the recipients like the advertisement and will share it with  friends on the following day. Assuming nobody receives the advertisement twice, determine how many people have liked the ad by the end of a given day, beginning with launch day as day 1.
+	
+	*/
+
+
+const viralAdverstising = n => {
+	let cumulate = [2]
+	for (let i = 1; i < n; i++){
+		let a = Math.floor((cumulate[i - 1] * 3) / 2);
+		cumulate.push(a)
+	}
+
+	let totalSum = 0;
+	cumulate.forEach(cum => totalSum += cum);
+
+	return totalSum
+}
+
+console.log(viralAdverstising(5));
